52d17c605912371a5ed52ddee6aa32c9b105bff5
ukewea <60734042+ukewea@users.noreply.github.com>
2021-05-01 20:17:22 +0800

    Sum Spot asset balances as USDT

--- crypto.py
+++ crypto.py
@@ -1,12 +1,13 @@
 from binance.client import Client
 from binance.enums import *
 
+
 class Crypto:
     # 建構式
     def __init__(self, config):
         self.client = Client(config.auth["API_KEY"], config.auth["API_SECRET"])
 
-    def get_exchange_symbol(self, quoteAsset):
+    def get_tradable_symbols(self, quoteAsset):
         exchange_info = self.client.get_exchange_info()
         # print(type(exchange_info))
         # with open('exchange_info.json', 'w') as outfile:
@@ -15,24 +16,24 @@ class Crypto:
         # 找出以 USDT 報價，且目前可交易、可送市價單、非槓桿型的交易對
         # print([item for item in exchange_info['symbols'] if item['symbol'] == "BTCUSDT"])
         trade_with_usdt = [item for item in exchange_info['symbols']
-                        if item['quoteAsset'].upper() == quoteAsset]
+                           if item['quoteAsset'].upper() == quoteAsset]
 
-        return [symbol['symbol'] for symbol in trade_with_usdt if symbol['status'].upper() == "TRADING" and "MARKET" in symbol['orderTypes']and not "LEVERAGED" in symbol['permissions']]
+        return [[symbol['symbol'], symbol['baseAsset']] for symbol in trade_with_usdt if symbol['status'].upper() == "TRADING" and "MARKET" in symbol['orderTypes'] and not "LEVERAGED" in symbol['permissions']]
 
-    def get_klines(self, symbol, klines_limit = 100, interval=Client.KLINE_INTERVAL_15MINUTE):
+    def get_klines(self, symbol, klines_limit=100, interval=Client.KLINE_INTERVAL_15MINUTE):
         """
         Get klines data
-        :param symbol: symbol e.q. BTCUSDT、DOGEUSDT
+        :param symbol: symbol e.g., BTCUSDT、DOGEUSDT
         :param klines_limit: K線的數量限制
         :param interval: 幾分的K線資料
         :return: klines data
         """
         klines = self.client.get_klines(
-        symbol=symbol,
-        interval=Client.KLINE_INTERVAL_15MINUTE,
-        limit=klines_limit)
+            symbol=symbol,
+            interval=Client.KLINE_INTERVAL_15MINUTE,
+            limit=klines_limit)
         if len(klines) < klines_limit:
-            raise Exception(f"No enough data for {symbol} {len(klines)}")
+            raise Exception(f"No enough data for {symbol} (only {len(klines)})")
 
         return [Kline(data) for data in klines]
 
@@ -41,22 +42,26 @@ class Crypto:
     #     return [kline.close for kline in klines]
 
 
+    def get_account(self):
+        account = self.client.get_account()
+        return account
+
 class Kline:
     def __init__(self, dict):
         """
         [
-        1499040000000,      # Open time
-        "0.01634790",       # Open
-        "0.80000000",       # High
-        "0.01575800",       # Low
-        "0.01577100",       # Close
-        "148976.11427815",  # Volume
-        1499644799999,      # Close time
-        "2434.19055334",    # Quote asset volume
-        308,                # Number of trades
-        "1756.87402397",    # Taker buy base asset volume
-        "28.46694368",      # Taker buy quote asset volume
-        "17928899.62484339" # Can be ignored
+            1499040000000,      # Open time
+            "0.01634790",       # Open
+            "0.80000000",       # High
+            "0.01575800",       # Low
+            "0.01577100",       # Close
+            "148976.11427815",  # Volume
+            1499644799999,      # Close time
+            "2434.19055334",    # Quote asset volume
+            308,                # Number of trades
+            "1756.87402397",    # Taker buy base asset volume
+            "28.46694368",      # Taker buy quote asset volume
+            "17928899.62484339" # Can be ignored
         ]
         """
         self.open_time = dict[0]
--- get_data.py
+++ get_data.py
@@ -6,19 +6,56 @@ from config import *
 tic = time.perf_counter()
 config = Config()
 crypto = Crypto(config)
-watching_symbols = crypto.get_exchange_symbol("USDT")
+
+account = crypto.get_account()
+balances = account['balances']
+
+watching_symbols = crypto.get_tradable_symbols("USDT")
 rsi_analyzer = RSI_Analyzer(config)
 willr_analyzer = WILLR_Analyzer(config)
-for symbol in watching_symbols:
+
+total_free_balance_as_usdt = 0.0
+total_locked_balance_as_usdt = 0.0
+
+for symbol_info in watching_symbols:
+    symbol = symbol_info[0]
+    base_asset = symbol_info[1]
+
+    asset_balance = [x for x in balances if x['asset'] == base_asset]
+    if len(asset_balance) < 1:
+        # 沒辦法看到該幣餘額，推斷帳號無法交易此幣，所以不計算策略
+        print(f"Cannot get {base_asset} balance in your account")
+        continue
+
     try:
         klines = crypto.get_klines(symbol)
+        latest_quote = float(klines[-1].close)
+        free_balance = float(asset_balance[0]['free'])
+        locked_balance = float(asset_balance[0]['locked'])
+
+        total_free_balance_as_usdt += free_balance * latest_quote
+        total_locked_balance_as_usdt += locked_balance * latest_quote
+
         trade = rsi_analyzer.Analyze(klines)
+        if trade == Trade.PASS:
+            continue
+
         print(f"[{symbol}]:")
         print(f"    RSI: {trade}")
         trade = willr_analyzer.Analyze(klines)
         print(f"    WILLR: {trade}")
     except Exception as e:
-        print(e.message)
+        print(e)
 
 toc = time.perf_counter()
 print(f"Download quotes & technical analysis took {toc - tic:0.4f} seconds")
+
+usdt_balance = [x for x in balances if x['asset'] == "USDT"]
+free_usdt = float(usdt_balance[0]['free'])
+locked_usdt = float(usdt_balance[0]['locked'])
+print(f"Your permission: {account['permissions']}")
+print(f"Your USDT balance info: {usdt_balance}")
+print(f"All free spot asset balance as USDT = {total_free_balance_as_usdt}"
+    f", +USDT = {total_free_balance_as_usdt + free_usdt}")
+print(f"All locked spot asset balance as USDT = {total_locked_balance_as_usdt}"
+    f", +USDT = {total_locked_balance_as_usdt + locked_usdt}")
