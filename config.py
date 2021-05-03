2fde562f5cf2a42c03baa42b9c1530953525d7bf
ukewea <60734042+ukewea@users.noreply.github.com>
2021-05-03 14:49:59 +0800

    加入倉位紀錄的加減法

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
         with open("auth.json", "r+") as json_file:
             self.auth = json.load(json_file)
 
-        with open("analyzer-parameters.json", "r+") as json_file2:
-            self.analyzer = json.load(json_file2)
+        with open("analyzer-parameters.json", "r+") as json_file:
+            self.analyzer = json.load(json_file)
